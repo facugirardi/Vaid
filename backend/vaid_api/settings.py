8a818bd07f126468b7a4e0432758864b92d0e57e
facugirardi <facugirardi22@gmail.com>
2024-05-14 18:08:18 -0300

    aws

--- backend/vaid_api/settings.py
+++ backend/vaid_api/settings.py
@@ -84,6 +84,19 @@ DATABASES = {
 }
 
 
+EMAIL_BACKEND = 'django_ses.SESBackend'
+DEFAULT_FROM_EMAIL = key.AWS_SES_FROM_EMAIL
+
+AWS_SES_ACCESS_KEY_ID = key.AWS_SES_ACCESS_KEY_ID
+AWS_SES_SECRET_ACCESS_KEY = key.AWS_SES_SECRET_ACCESS_KEY
+AWS_SES_REGION_NAME = key.AWS_SES_REGION_NAME
+AWS_SES_REGION_ENDPOINT = f'email.{AWS_SES_REGION_NAME}.amazonaws.com'
+AWS_SES_FROM_EMAIL = key.AWS_SES_FROM_EMAIL
+
+USE_SES_V2 = True
+
+SITE_NAME = 'Vaid'
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
