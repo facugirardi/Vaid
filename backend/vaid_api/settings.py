36162514b87d1d3759f8edbb02b6d1e9c95d5bc8
facugirardi <facugirardi22@gmail.com>
2024-05-14 23:59:14 -0300

    oauth

--- backend/vaid_api/settings.py
+++ backend/vaid_api/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'users',
-    'djoser'
+    'social_django',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -143,12 +144,45 @@ DJOSER = {
     'USER_CREATE_PASSWORD_RETYPE': True,
     'PASSWORD_RESET_CONFIRM_RETYPE': True,
     'TOKEN_MODEL': None,
+    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': [
+        'http://localhost:3000/auth/google',
+        'http://localhost:3000/auth/facebook'
+    ]
 }
 
 AUTH_COOKIE = 'access'
+AUTH_COOKIE_MAX_AGE = 60 * 60 * 24
+AUTH_COOKIE_SECURE = False # Cambiar a TRUE en produccion
+AUTH_COOKIE_HTTP_ONLY = True
+AUTH_COOKIE_PATH = '/'
+AUTH_COOKIE_SAMESITE = 'None'
 
 CORS_ALLOWED_ORIGINS = key.CORS_ALLOWED_ORIGINS
 CORS_ALLOW_CREDENTIALS = True
+
+
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = key.GOOGLE_AUTH_KEY
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = key.GOOGLE_AUTH_SECRET_KEY
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'openid'
+]
+SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ['first_name', 'last_name']
+
+SOCIAL_AUTH_FACEBOOK_KEY = key.FACEBOOK_AUTH_KEY
+SOCIAL_AUTH_FACEBOOK_SECRET = key.FACEBOOK_AUTH_SECRET_KEY
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+    'fields': 'email, first_name, last_name'
+}
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
