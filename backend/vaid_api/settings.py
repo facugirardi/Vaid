10274ca8ffb5a6f5af25b535be4c71569c9141e5
Budi <alejo.vaquero@hotmail.com>
2024-05-23 10:41:05 -0300

    validator finished

--- backend/vaid_api/settings.py
+++ backend/vaid_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import key 
+import key
 from pathlib import Path
 from vaid_api import validator
 
