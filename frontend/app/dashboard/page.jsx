17eff22c31fc7f7d4c7903aa3e268b3a840f4c76
facugirardi <facugirardi22@gmail.com>
2024-05-30 08:25:42 -0300

    isLoading

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -3,6 +3,7 @@
 import LandingLayout from "@/layouts/LandingLayout";
 import { useEffect, useState } from "react";
 import { useRetrieveUserQuery } from "@/redux/features/authApiSlice";
+import Preloader from "@/components/Preloader";
 
 const breaks = Array(10).fill(0).map((_, i) => <br key={i} />); // borrar cuando se haga el login
 
@@ -16,22 +17,26 @@ const page = () => {
 
   }, []);
 
-
-
-
   return (
-    <LandingLayout header footer bodyClass={"home-three"} onePage>
-      <section className="hero-area-three bgs-cover bgc-lighter pt-210 rpt-150 pb-100">
-        <div className="container">
-          <div className="d-flex justify-content-center">
-            <p>
-              {user?.first_name} {user?.last_name}, {user?.email} 
-            </p>
-            {breaks}
-          </div>
-        </div>
-      </section>
-    </LandingLayout>
+    <>
+    {isLoading || isFetching 
+      ? <Preloader />
+      : (
+        <LandingLayout header footer bodyClass="home-three" onePage>
+          <section className="hero-area-three bgs-cover bgc-lighter pt-210 rpt-150 pb-100">
+            <div className="container">
+              <div className="d-flex justify-content-center">
+                <p>
+                  ID: {user?.id} | Name: {user?.first_name} {user?.last_name} | Email: {user?.email} 
+                </p>
+                {breaks}
+              </div>
+            </div>
+          </section>
+        </LandingLayout>
+      )
+    }
+  </>  
   );
 };
 
