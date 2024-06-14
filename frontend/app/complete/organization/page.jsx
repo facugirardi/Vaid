133b61f7d239320b66b1ed62a6ad462154a1d3f7
facugirardi <facugirardi22@gmail.com>
2024-06-14 15:03:42 -0300

    orgcom

--- frontend/app/complete/organization/page.jsx
+++ frontend/app/complete/organization/page.jsx
@@ -15,17 +15,54 @@ const page = () => {
     const { data: user } = useRetrieveUserQuery();
 
   return (
+
+
+      
+
+
     <LandingLayout header footer bodyClass={"home-three"} onePage>
       <section
         className="hero-area-three bgs-cover bgc-lighter pt-210 rpt-150 pb-100"
       >
+                <div className="container">
+          <div className="d-flex justify-content-center">
+
+              <div className='wrapper-complete-user'>
+            <form >
+                <div className='flex-item-logo'>
+                    <img src="/assets/images/vaidpng3.png" alt="" className='imgLogo_login'/>
+                </div>
+
+                <div className="input-box flex-item">
+                    <label className='label_input'>Organization Name</label>
+                    <input name='name' type="text" placeholder='Enter the name of your organization' required />
+                </div>
 
 
-        
+                <div className="input-box flex-item">
+                    <label className='label_input'>Website (Optional)</label>
+                    <input name='website' type="text" placeholder='Enter your website' />
+                </div>
+
+                <div className="input-box flex-item">
+                    <label className='label_input'>Description</label>
+                    <input name='description' className='description-org' type="text" placeholder='Enter a description' required />
+                </div>
+                <div className='flex-item'>
+                <button type="submit" >Continue</button>
+                </div>
+                
+            </form>
+        </div>
+        </div>
+        </div>
       </section>
       {breaks}    
     </LandingLayout>
+
+
+
+
   );
 }
 export default page;
-
--- frontend/app/complete/user/page.jsx
+++ frontend/app/complete/user/page.jsx
@@ -14,18 +14,50 @@ const page = () => {
     const { push } = useRouter();
     const { data: user } = useRetrieveUserQuery();
 
+const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <LandingLayout header footer bodyClass={"home-three"} onePage>
       <section
         className="hero-area-three bgs-cover bgc-lighter pt-210 rpt-150 pb-100"
       >
+                <div className="container">
+          <div className="d-flex justify-content-center">
+
+              <div className='wrapper-complete-user'>
+            <form onSubmit={onSubmit}>
+                <div className='flex-item-logo'>
+                    <img src="/assets/images/vaidpng3.png" alt="" className='imgLogo_login'/>
+                </div>
+
+                <div className="input-box flex-item">
+                    <label className='label_input'>Email</label>
+                    <input name='email' type="email" placeholder='Enter your email' required />
+                </div>
 
 
-        
+                <div className="input-box flex-item">
+                    <label className='label_input'>Email</label>
+                    <input name='email' type="email" placeholder='Enter your email' required />
+                </div>
+
+                <div className="input-box flex-item">
+                    <label className='label_input'>Password</label>
+                    <input name='password' type="password" placeholder='Enter your password' required />
+                </div>
+                <div className='flex-item'>
+                <button type="submit" >Login</button>
+                </div>
+                
+            </form>
+        </div>
+        </div>
+        </div>
       </section>
       {breaks}    
     </LandingLayout>
   );
 }
 export default page;
-
