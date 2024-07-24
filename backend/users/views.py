ed172cbd5b30d507884b0f894b964c0c8439f077
lucasgalleguillo <galleguillolucas2006@gmail.com>
2024-07-23 23:32:16 -0300

    View Tasks

--- backend/users/views.py
+++ backend/users/views.py
@@ -394,3 +394,47 @@ class LogoutView(APIView):
         response.delete_cookie('refresh')
 
         return response
+
+
+# tasksViews
+class TaskListCreateView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskRetrieveUpdateDestroyView(APIView):
+    permission_classes = [AllowAny]
+
+    def get_object(self, pk):
+        try:
+            return Task.objects.get(id=pk)
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
