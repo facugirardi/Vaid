f07bf8928e0dab42ec07d668ea80fa06a711286c
joacohlavach <joaquin.hlavach@gmail.com>
2024-08-02 11:36:42 -0300

    a

--- frontend/app/dashboard/[organization]/tasks/create/page.jsx
+++ frontend/app/dashboard/[organization]/tasks/create/page.jsx
@@ -1,19 +1,28 @@
 // page.jsx
 'use client'
 
-import React,  { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import Layout from '@/layouts/dashboard/index';
 import { useRetrieveUserQuery } from '@/redux/features/authApiSlice';
-import { Card, Button, Form, InputGroup, Col, Row, Link} from 'react-bootstrap';
+import { Card, Button, Form, Col, Row } from 'react-bootstrap';
 import BreadcrumbItem from '@/common/BreadcrumbItem';
-import { useState } from 'react';
+import FeatherIcon from "feather-icons-react";
 import './create.css';
-
-
-
 
 const page = () => {
     const { data: user, isLoading, isError } = useRetrieveUserQuery();
+    const [file, setFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+
+    const onFileChange = event => {
+        if (event.target.files && event.target.files[0]) {
+            setFile(event.target.files[0]);
+            setPreview(URL.createObjectURL(event.target.files[0]));
+        } else {
+            setFile(null);
+            setPreview(null);
+        }
+    };
 
     if (isLoading) return <p>Loading...</p>;
     if (isError || !user) return <p>Error loading user data!</p>;
@@ -22,62 +31,91 @@ const page = () => {
         <Layout>
             <BreadcrumbItem mainTitle="Dashboard" subTitle="Task" />
             <Row>
-            <Card>
-                <div id="sticky-action" className="sticky-action">
-                    <Card.Header>
-                        <Row className="align-items-center">
-                            <Col sm={6}>
-                                <h4>Create Task</h4>
-                            </Col>
-                            <Col sm={6} className="text-sm-end mt-3 mt-sm-0">
-                                <Button variant="success" type="reset">
-                                    Submit
-                                </Button>
-                                <Button variant="light-secondary" type="reset">
-                                    Cancel
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Card.Header>
-                </div>
-                
-                <Card.Body>
-
-                    <h5>Form controls</h5>
-                    <hr />
-                    <Row>
-                        <Col md={6}>
-                            <Form onSubmit={(event) => event.preventDefault()}>
-                                <div className="form-group">
-                                    <Col md={6}>
-                                    <Form.Label>Title</Form.Label>
-                                    <Form.Control
-                                        type="title"
-                                        placeholder="Enter a title"
-                                    />
-                                    <Form.Label>Description</Form.Label>
-                                    <Form.Control
-                                        type="description"
-                                        placeholder="Enter a description"
-                                    />
-                                    <label className="form-label">Category</label>
-                                        <select className="form-select">
-                                        <option>a</option>
-                                        <option>b</option>
-                                        <option>c</option>
-                                        </select>
-                                    </Col>
-                            
-                                </div>
-
-                            </Form>
-                        </Col>
-                    </Row>
-                </Card.Body>
-            </Card>
-
+                <Card>
+                    <div id="sticky-action" className="sticky-action">
+                        <Card.Header>
+                            <Row className="align-items-center">
+                                <Col sm={6}>
+                                    <h4>Create Task</h4>
+                                </Col>
+                                <Col sm={6} className="text-sm-end mt-3 mt-sm-0">
+                                    <Button variant="success" type="reset" className="botontask">
+                                        Submit
+                                    </Button>
+                                    <Button variant="light-secondary" type="reset" className="botontask">
+                                        Cancel
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Card.Header>
+                    </div>
+                    
+                    <Card.Body>
+                        <Form onSubmit={(event) => event.preventDefault()}>
+                            <Row className="mb-3">
+                                <Col md={4} className="d-flex flex-column align-items-center">
+                                    <label htmlFor="upload-button" className="upload-button">
+                                        {preview ? (
+                                            <img src={preview} alt="Preview" className="preview-img" />
+                                        ) : (
+                                            <div className="icon-container">
+                                                <FeatherIcon icon="upload" />
+                                            </div>
+                                        )}
+                                        <input id="upload-button" type="file" onChange={onFileChange} style={{ display: 'none' }} />
+                                    </label>
+                                </Col>
+                                <Col md={8}>
+                                    <Form.Group>
+                                        <Form.Label className="form-group-label">Title</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter a title"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label className="form-group-label">Description</Form.Label>
+                                        <Form.Control
+                                            className="textarea-task"
+                                            as="textarea"
+                                            rows={3}
+                                            placeholder="Enter a description"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Form.Group>
+                                <Form.Label className="form-label-2">Category</Form.Label>
+                                <Form.Control as="select" className="form-select">
+                                    <option>a</option>
+                                    <option>b</option>
+                                    <option>c</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Row className="form-group-2">
+                                <Col sm={6} md={3}>
+                                    <Form.Label className="form-label-2">Start Date</Form.Label>
+                                    <Form.Control type="date" />
+                                </Col>
+                                <Col sm={6} md={3}>
+                                    <Form.Label className="form-label-2">End Date</Form.Label>
+                                    <Form.Control type="date" />
+                                </Col>
+                                <Col sm={6} md={3}>
+                                    <Form.Label className="form-label-2">Start Time</Form.Label>
+                                    <Form.Control type="time" />
+                                </Col>
+                                <Col sm={6} md={3}>
+                                    <Form.Label className="form-label-2">End Time</Form.Label>
+                                    <Form.Control type="time" />
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Card.Body>
+                </Card>
             </Row>
         </Layout>
     ); 
 } 
+
 export default page;
