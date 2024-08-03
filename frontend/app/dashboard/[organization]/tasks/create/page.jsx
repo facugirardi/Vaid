047b3167bcec0d2e3ef7f533fdad1179f41770d2
Facundo Girardi <82075452+facugirardi@users.noreply.github.com>
2024-08-03 00:44:59 -0300

    Update page.jsx

--- frontend/app/dashboard/[organization]/tasks/create/page.jsx
+++ frontend/app/dashboard/[organization]/tasks/create/page.jsx
@@ -1,58 +1,121 @@
 // page.jsx
 'use client'
 
-import React, { ReactElement, useEffect, useState } from "react";
-import Layout from '@/layouts/dashboard/index2';
-import BreadcrumbItem from '@/common/BreadcrumbItem';
+import React, { ReactElement, useState } from "react";
+import Layout from '@/layouts/dashboard/index';
 import { useRetrieveUserQuery } from '@/redux/features/authApiSlice';
-import { ProgressBar, TabContainer, Col, Form, Card, Nav, Row, Tab } from "react-bootstrap";
-import './form.css';
-import ava2 from "@/public/assets/images/user/avatar-2.jpg";
-import Image from "next/image";
+import { Card, Button, Form, Col, Row } from 'react-bootstrap';
+import BreadcrumbItem from '@/common/BreadcrumbItem';
+import FeatherIcon from "feather-icons-react";
+import './create.css';
 
-const Page = () => {
+const page = () => {
+    const { data: user, isLoading, isError } = useRetrieveUserQuery();
+    const [file, setFile] = useState(null);
+    const [preview, setPreview] = useState(null);
 
-
-    const { data: user, isError, isLoading } = useRetrieveUserQuery();
-    const [key, setKey] = useState('tab-1');
-    const [progress, setProgress] = useState(25);
-    const totalTabs = 4;
-
-    useEffect(() => {
-        // Calculate progress when the component mounts
-        const calculateProgress = () => {
-          return ((parseInt(key.split('-')[1]) - 1) / (totalTabs - 1)) * 100;
-        };
-        setProgress(calculateProgress());
-      }, [key, totalTabs]);
-
-    const handleTabSelect = (k) => {
-        setKey(k);
+    const onFileChange = event => {
+        if (event.target.files && event.target.files[0]) {
+            setFile(event.target.files[0]);
+            setPreview(URL.createObjectURL(event.target.files[0]));
+        } else {
+            setFile(null);
+            setPreview(null);
+        }
     };
-
-    const handleNext = () => {
-        const nextKey = parseInt(key.split('-')[1]) + 1;
-        setKey(`tab-${nextKey}`);
-    };
-
-    const handlePrevious = () => {
-        const previousKey = parseInt(key.split('-')[1]) - 1;
-        setKey(`tab-${previousKey}`);
-    };
-
-
-    
 
     if (isLoading) return <p>Loading...</p>;
     if (isError || !user) return <p>Error loading user data!</p>;
 
     return (
         <Layout>
+            <BreadcrumbItem mainTitle="Dashboard" subTitle="Task" />
             <Row>
-                <Col>
-                    <BreadcrumbItem />
-                </Col>
+                <Card>
+                    <div id="sticky-action" className="sticky-action">
+                        <Card.Header>
+                            <Row className="align-items-center">
+                                <Col sm={6}>
+                                    <h4>Create Task</h4>
+                                </Col>
+                                <Col sm={6} className="text-sm-end mt-3 mt-sm-0">
+                                    <Button variant="success" type="reset" className="botontask">
+                                        Submit
+                                    </Button>
+                                    <Button variant="light-secondary" type="reset" className="botontask">
+                                        Cancel
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Card.Header>
+                    </div>
+                    
+                    <Card.Body>
+                        <Form onSubmit={(event) => event.preventDefault()}>
+                            <Row className="mb-3">
+                                <Col md={4} className="d-flex flex-column align-items-center">
+                                    <label htmlFor="upload-button" className="upload-button">
+                                        {preview ? (
+                                            <img src={preview} alt="Preview" className="preview-img" />
+                                        ) : (
+                                            <div className="icon-container">
+                                                <FeatherIcon icon="upload" />
+                                            </div>
+                                        )}
+                                        <input id="upload-button" type="file" onChange={onFileChange} style={{ display: 'none' }} />
+                                    </label>
+                                </Col>
+                                <Col md={8}>
+                                    <Form.Group>
+                                        <Form.Label className="form-group-label">Title</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter a title"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label className="form-group-label">Description</Form.Label>
+                                        <Form.Control
+                                            className="textarea-task"
+                                            as="textarea"
+                                            rows={3}
+                                            placeholder="Enter a description"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Form.Group>
+                                <Form.Label className="form-label-2">Category</Form.Label>
+                                <Form.Control as="select" className="form-select">
+                                    <option>a</option>
+                                    <option>b</option>
+                                    <option>c</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Row className="form-group-2">
+                                <Col sm={6} md={3}>
+                                    <Form.Label className="form-label-2">Start Date</Form.Label>
+                                    <Form.Control type="date" />
+                                </Col>
+                                <Col sm={6} md={3}>
+                                    <Form.Label className="form-label-2">End Date</Form.Label>
+                                    <Form.Control type="date" />
+                                </Col>
+                                <Col sm={6} md={3}>
+                                    <Form.Label className="form-label-2">Start Time</Form.Label>
+                                    <Form.Control type="time" />
+                                </Col>
+                                <Col sm={6} md={3}>
+                                    <Form.Label className="form-label-2">End Time</Form.Label>
+                                    <Form.Control type="time" />
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Card.Body>
+                </Card>
             </Row>
         </Layout>
     ); 
-}
+} 
+
+export default page;
