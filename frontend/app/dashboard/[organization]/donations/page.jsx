71009a864d14ab95c9313e7bc915068db318bb83
FabricioMendez <fabriciolagloria@gmail.com>
2024-08-13 09:29:02 -0300

    Donations

---
+++ frontend/app/dashboard/[organization]/donations/page.jsx
@@ -0,0 +1,111 @@
+"use client";
+
+import React from 'react';
+import './donation.css';
+import Layout from '@/layouts/dashboard/index';
+import BreadcrumbItem from '@/common/BreadcrumbItem';
+
+const BuySell = () => {
+  return (
+  <div className="container">
+  <div className="cards-container">
+  <div className="card">
+  <div className="card-header">
+    <h3>Buy and Sell</h3>
+    <button className="add-button">+</button>
+  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Units</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Operation</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+      </tr>
+      <tr>
+        <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+      </tr>
+      <tr>
+        <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+      </tr>
+      <tr>
+        <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+      </tr>
+      <tr>
+        <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+</div>)
+}
+
+const Donations = () => {
+  return (
+    <div className="container">
+      <div className="cards-container">
+        <div className="card">
+          <div className="card-header">
+            <h3>Donations</h3>
+            <button className="add-button">+</button>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Units</th>
+                <th>Income Date</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+              </tr>
+              <tr>
+                <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+              </tr>
+              <tr>
+                <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+              </tr>
+              <tr>
+                <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+              </tr>
+              <tr>
+                <td><button className="options-button"><i className="fas fa-ellipsis-v"></i></button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const Page = () => {
+  return (
+    <Layout>
+      <BreadcrumbItem mainTitle="Resource Management" subTitle="Donations" />
+      <div className="container">
+
+        <div className='row'>
+          <div className="col-md-6">
+            <Donations /> 
+          </div>
+          <div className="col-md-6">
+            <BuySell /> 
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Page;
