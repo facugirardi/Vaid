0125af62e8360c14e540c8f8bdc3b87d457cd313
facugirardi <facugirardi22@gmail.com>
2024-08-16 12:55:03 -0300

    cambios_historial

--- backend/users/views.py
+++ backend/users/views.py
@@ -944,8 +944,8 @@ class OrganizationHistoryView(APIView):
             # Verificar que la organización existe
             organization = Organization.objects.get(id=organization_id)
 
-            # Filtrar el historial de acciones de la ONG
-            history_records = History.objects.filter(headquarter_id__Organization=organization)
+            # Filtrar el historial de acciones de la ONG usando el campo headquarter_id
+            history_records = History.objects.filter(Organization=organization)
             serializer = HistorySerializer(history_records, many=True)
 
             return Response(serializer.data, status=status.HTTP_200_OK)
@@ -955,22 +955,19 @@ class OrganizationHistoryView(APIView):
 
     def post(self, request, organization_id):
         serializer = HistorySerializer(data=request.data)
-
+        print(request.data)
+        print(serializer)
         if serializer.is_valid():
-            user_id = serializer.validated_data.get('user_id')
-            headquarter_id = serializer.validated_data.get('headquarter_id')
 
             # Verificar que el usuario y la sede existen
             try:
-                user = UserAccount.objects.get(id=user_id)
-                headquarter = Headquarter.objects.get(id=headquarter_id)
+                organization = Organization.objects.get(id=organization_id)
 
                 # Crear el historial
                 history = History.objects.create(
-                    user_id=user,
                     action=serializer.validated_data['action'],
                     description=serializer.validated_data['description'],
-                    headquarter_id=headquarter
+                    Organization=organization
                 )
 
                 return Response({'message': 'Action recorded successfully', 'id': history.id}, status=status.HTTP_201_CREATED)
@@ -980,8 +977,7 @@ class OrganizationHistoryView(APIView):
             except Headquarter.DoesNotExist:
                 return Response({'error': 'Headquarter not found'}, status=status.HTTP_404_NOT_FOUND)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
-
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProductCreateView(APIView):
     permission_classes = [AllowAny]
