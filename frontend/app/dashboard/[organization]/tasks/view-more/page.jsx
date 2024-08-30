0ff4a0d4bea659ba7be50418a207a7f7ba4e327e
FabricioMendez <fabriciolagloria@gmail.com>
2024-08-30 10:21:03 -0300

    View-tasks

--- frontend/app/dashboard/[organization]/tasks/view-more/page.jsx
+++ frontend/app/dashboard/[organization]/tasks/view-more/page.jsx
@@ -4,8 +4,9 @@ import React, { useState, useEffect } from "react";
 import Layout from '@/layouts/dashboard/index';
 import BreadcrumbItem from '@/common/BreadcrumbItem';
 import Image from "next/image";
-import { Card, Col, Form, Row } from "react-bootstrap";
+import { Card, Col, Form, Row, Button } from "react-bootstrap";
 import cover1 from "@/public/assets/images/wallpaper_event.jpg";
+import './view-more.css'; // Asegúrate de importar los estilos
 
 const Page = () => {
     const [tasks, setTasks] = useState([]);
@@ -26,7 +27,7 @@ const Page = () => {
         if (organizationId) {
             const fetchData = async () => {
                 try {
-                  const response = await fetch(`http://localhost:8000/api/organizations/${organizationId}/events`, {
+                    const response = await fetch(`http://localhost:8000/api/organizations/${organizationId}/events`, {
                         method: 'GET',
                         headers: {
                             'Content-Type': 'application/json'
@@ -47,56 +48,72 @@ const Page = () => {
         setSelectedTask(task);
     };
 
+    const handleCreateClick = () => {
+        // Lógica para el botón Create
+        console.log('Create button clicked');
+    };
+
+    const handleCloseClick = () => {
+        // Lógica para el botón Close
+        console.log('Close button clicked');
+    };
+
     return (
         <Layout>
             <div className="header">
-                <BreadcrumbItem mainTitle="Taks" subTitle="View Tasks" />
+                <BreadcrumbItem mainTitle="Tasks" subTitle="View Tasks" />
             </div>
-                <div className="d-flex justify-content-center mt-4">
-                    <div className="container event-details-container p-4 border rounded">
-                        <div className="row">
-                            <div className="image-container col-12 col-md-5">
-                                <Image 
-                                    src={cover1} 
-                                    alt="image" 
-                                    className="img-popup-event" 
-                                    width={480} 
-                                    height={305}
-                                />
-                            </div>
-                            <div className="details-container col-md-7">
-                                <p className='title-modal-12'>Title</p>
-                                <p className='title2-modal'>{selectedTask?.name || 'No title available'}</p>
-                                <p className='title-modal-12'>Description</p>
-                                <p className='title3-modal'>{selectedTask?.description || 'No description available'}</p>
-                                <p className='title-modal-12'>Location</p>
-                                <p className='title3-modal'>{selectedTask?.location || 'No location available'}</p>
-                                <Form.Group className='form-group-all'>
-                                    <div className="row">
-                                        <div className='col-md-3'>
-                                            <p className='title-dates'>Start Date</p>
-                                            <Form.Control type="date"  />
-                                        </div>
-                                        <div className="col-md-3">
-                                            <p className='title-dates'>End Date</p>
-                                            <Form.Control type="date"  />
-                                        </div>
-                                        <div className="col-md-3">
-                                            <p className='title-dates'>Start Time</p>
-                                            <Form.Control type="time" />
-                                        </div>
-                                        <div className="col-md-3">
-                                            <p className='title-dates'>End Time</p>
-                                            <Form.Control type="time" />
-                                        </div>
+            <div className="d-flex justify-content-center mt-4">
+                <div className="container event-details-container p-4 border rounded">
+                    <div className="row">
+                        <div className="image-container col-12 col-md-5">
+                            <Image 
+                                src={cover1} 
+                                alt="image" 
+                                className="img-popup-event" 
+                                width={480} 
+                                height={305}
+                            />
+                        </div>
+                        <div className="details-container col-md-7">
+                            <p className='title-modal-12'>Title</p>
+                            <p className='title2-modal'>{selectedTask?.name || 'No title available'}</p>
+                            <p className='title-modal-12'>Description</p>
+                            <p className='title3-modal'>{selectedTask?.description || 'No description available'}</p>
+                            <p className='title-modal-12'>Location</p>
+                            <p className='title3-modal'>{selectedTask?.location || 'No location available'}</p>
+                            <Form.Group className='form-group-all'>
+                                <div className="row">
+                                    <div className='col-md-3'>
+                                        <p className='title-dates'>Start Date</p>
+                                        <Form.Control type="date" />
                                     </div>
-                                </Form.Group>
-
+                                    <div className="col-md-3">
+                                        <p className='title-dates'>End Date</p>
+                                        <Form.Control type="date" />
+                                    </div>
+                                    <div className="col-md-3">
+                                        <p className='title-dates'>Start Time</p>
+                                        <Form.Control type="time" />
+                                    </div>
+                                    <div className="col-md-3">
+                                        <p className='title-dates'>End Time</p>
+                                        <Form.Control type="time" />
+                                    </div>
+                                </div>
+                            </Form.Group>
+                            <div className="task-buttons mt-3">
+                                <button className="create-button" onClick={handleCreateClick}>
+                                    Create
+                                </button>
+                                <button className="close-button" onClick={handleCloseClick}>
+                                    Close
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
-                
+            </div>
         </Layout>
     );
 };
