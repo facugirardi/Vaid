b32f149aa93ebecb92d1734ec6acd74ada87b79d
facugirardi <facugirardi22@gmail.com>
2024-10-23 10:30:29 -0300

    fix

--- backend/vaid_api/settings.py
+++ backend/vaid_api/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'vaid_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'defaultdb',
-        'USER':'avnadmin',
-        'PASSWORD':key.DBPASSWORD,
-        'HOST':'vaid-facugirardi22-96f8.g.aivencloud.com',
-        'PORT':'24856'
+        "ENGINE": "django.db.backends.sqlite3",
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
