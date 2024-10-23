18d5167328bc2746fb0c80fc14b7f1e04476bd37
facugirardi22@gmail.com <facugirardi22@gmail.com>
2024-10-22 23:57:29 -0300

    base de datos postgre

--- backend/vaid_api/settings.py
+++ backend/vaid_api/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'vaid_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'defaultdb',
+        'USER':'avnadmin',
+        'PASSWORD':'AVNS_bHw6_EccGwFVjcfcpnr',
+        'HOST':'vaid-facugirardi22-96f8.g.aivencloud.com',
+        'PORT':'24856'
     }
 }
-
 
 EMAIL_BACKEND = 'django_ses.SESBackend'
 DEFAULT_FROM_EMAIL = key.AWS_SES_FROM_EMAIL
