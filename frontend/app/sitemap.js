7015ed9172685b75caeaaf03e7d96481509b87cb
facugirardi22@gmail.com <facugirardi22@gmail.com>
2024-11-15 19:45:05 -0300

    sitemap

---
+++ frontend/app/sitemap.js
@@ -0,0 +1,10 @@
+ export default function sitemap() {
+  return [
+    {
+      url: 'https://www.vaidteam.com/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ]
+}
