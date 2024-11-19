e0f83f0f2cbc964489a862ef66430c331922a309
Facundo Girardi <82075452+facugirardi@users.noreply.github.com>
2024-11-18 21:47:43 -0300

    Update robots.js

--- frontend/app/robots.js
+++ frontend/app/robots.js
@@ -1,12 +1,12 @@
-import type { MetadataRoute } from 'next'
- 
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
     sitemap: 'https://vaidteam.com/sitemap.xml',
-  }
+  };
 }
